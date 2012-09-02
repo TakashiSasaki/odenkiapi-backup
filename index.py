c61656cd00adf845619e04412382a2cd3490c5d9
Takashi SASAKI <takashi316@gmail.com>
2012-09-02 19:44:14 +0900

    index.py was merged with master-merging branch

--- index.py
+++ index.py
@@ -8,14 +8,21 @@ from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.api import users
 from MyRequestHandler import MyRequestHandler
+#from lib.CachedContent import CachedContent
+#rom google.appengine.ext.webapp import RequestHandler
 
+#class MainPage(RequestHandler):
 class MainPage(MyRequestHandler):
 
     def get(self):
         self.response.headers['Content-Type'] = 'text/html'
         #self.response.out.write('Hello, webapp World!')
         current_user = users.GetCurrentUser()
-        template_values = {}
+        
+        #cached_content = CachedContent("/", {}, "html/index.html")
+        #cached_content.render()
+        #cached_content.write(self)
+        #template_values = {}
         self.writeWithTemplate(template_values, "index")
 
 if __name__ == "__main__":
