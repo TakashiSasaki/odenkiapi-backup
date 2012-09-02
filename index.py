351c6b61279735beefb8a06f17c44f59a87be4a6
Takashi SASAKI <takashi316@gmail.com>
2012-09-02 19:47:23 +0900

    index.py was merged with nhk-merging branch

--- index.py
+++ index.py
@@ -7,22 +7,25 @@ import logging
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.api import users
-#from MyRequestHandler import MyRequestHandler
-from lib.CachedContent import CachedContent
-from google.appengine.ext.webapp import RequestHandler
+from MyRequestHandler import MyRequestHandler
+#from lib.CachedContent import CachedContent
+#rom google.appengine.ext.webapp import RequestHandler
 
-class MainPage(RequestHandler):
+#class MainPage(RequestHandler):
+class MainPage(MyRequestHandler):
 
     def get(self):
         self.response.headers['Content-Type'] = 'text/html'
         #self.response.out.write('Hello, webapp World!')
         current_user = users.GetCurrentUser()
         
-        cached_content = CachedContent("/", {}, "html/index.html")
-        cached_content.render()
-        cached_content.write(self)
+        #cached_content = CachedContent("/", {}, "html/index.html")
+        #cached_content.render()
+        #cached_content.write(self)
+        #template_values = {}
+        self.writeWithTemplate(template_values, "index")
 
 if __name__ == "__main__":
     logging.getLogger().setLevel(logging.DEBUG)
-    application = webapp.WSGIApplication([('/', MainPage)], debug=True)
+    application = webapp.WSGIApplication([('/admin', MainPage)], debug=True)
     run_wsgi_app(application)
