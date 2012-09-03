700845f9cba673e8d2afb2e24ef814a3179596b8
Takashi SASAKI <takashi316@gmail.com>
2012-09-04 03:03:47 +0900

    implementing /api/echo

--- api/Echo.py
+++ api/Echo.py
@@ -1,16 +1,28 @@
 from lib.RequestDispatcher import RequestDispatcher
+from google.appengine.ext.webapp import RequestHandler
+import logging
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+logger.debug("Echo.py was loaded")
 
-class Echo(RequestDispatcher):
+#class Echo(RequestDispatcher):
+class Echo(RequestHandler):
     """Echo returns given RPC object as it is."""
-    def __init__(self):
-        RequestDispatcher.__init__(self)
+    #def __init__(self):
+    #    RequestDispatcher.__init__(self)
     
     def echo(self, rpc):
         rpc.result = rpc.getRequest()
     
     def post(self):
-        self.get()
+        self.request.out.write("echo")
+        #self.get()
+        
+    def get(self):
+        self.request.out.write("echo")
+        
 
 from lib import runWsgiApp
 if __name__ == "__main__":
-    runWsgiApp(Echo, "/api/Echo")
+    logger.debug("abc")
+    runWsgiApp(Echo, "/api/echo")
