85e1e7f5c4c4242f89fff4283862567e71a2ce2f
Takashi SASAKI <takashi316@gmail.com>
2012-09-08 11:52:06 +0900

    /api/session

--- api/session.py
+++ api/session.py
@@ -1,37 +1,51 @@
-from google.appengine.ext.webapp import RequestHandler
-import lib
+#import lib
+from lib.JsonRpc import *
 from google.appengine.api.users import create_login_url, create_logout_url
+from gaesessions import get_current_session, Session, set_current_session
+from lib.OdenkiSession import OdenkiSession
 
-class UserInfo(RequestHandler):
+class UserInfo(JsonRpcDispatcher):
     
-    __slot__ = ["jsonRpc"]
+    def GET(self, json_rpc_request):
+        assert isinstance(json_rpc_request, JsonRpcRequest)
+        json_rpc_response = JsonRpcResponse(json_rpc_request.getId())
+        json_rpc_response.setResultValue("gaesession", str(get_current_session()))
+        json_rpc_response.setResultValue("OdenkiSession", OdenkiSession())
+        return json_rpc_response
+
+#        odenki_session = lib.OdenkiSession()
+#        self.jsonRpc.setResultValule("sid", odenki_session.getSid())
+#       odenki_user = odenki_session.getOdenkiUser()
+#        if odenki_user:
+#            self.jsonRpc.updateResult(odenki_user.getDictionary())
+#        google_user = odenki_session.getGoogleUser()
+#        if google_user:
+#            self.jsonRpc.updateResult(google_user.getDictionary())
+#        self.jsonRpc.write()
+#        return
+
+    def terminate(self, json_rpc_request):
+        assert isinstance(json_rpc_request, JsonRpcRequest)
+        json_rpc_response = JsonRpcResponse(json_rpc_request.getId())
+        json_rpc_response.setResultValue("before", str(OdenkiSession().gaesession()))
+        OdenkiSession().gaesession().terminate()
+        json_rpc_response.setResultValue("after", str(OdenkiSession().gaesession()))
+        return json_rpc_response
+
+    @classmethod
+    def _getLoginUrl(self):
+        login_url = create_login_url("/api/session")
+        return login_url
     
-    def get(self):
-        self.jsonRpc = lib.JsonRpc(self)
-        self.setLoginLogoutUrl()
+    @classmethod
+    def _getLogoutUrl(self):
+        logout_url = create_logout_url("/api/session")
+        return logout_url
 
-        odenki_session = lib.OdenkiSession()
-        self.jsonRpc.setResultValule("sid", odenki_session.getSid())
-        odenki_user = odenki_session.getOdenkiUser()
-        if odenki_user:
-            self.jsonRpc.updateResult(odenki_user.getDictionary())
-        google_user = odenki_session.getGoogleUser()
-        if google_user:
-            self.jsonRpc.updateResult(google_user.getDictionary())
-        self.jsonRpc.write()
-        return
-
-    def setLoginLogoutUrl(self):
-        callback = self.jsonRpc.getParam("callback")
-        if callback:
-            login_url = create_login_url(callback)
-            self.jsonRpc.setResultValule("loginUrl", login_url)
-            logout_url = create_logout_url(callback)
-            self.jsonRpc.setResultValule("logoutUrl", logout_url)
-    
-    def post(self):
-        self.get()
-
-from lib import runWsgiApp
 if __name__ == "__main__":
-    runWsgiApp(UserInfo, "/api/SessionInfo")
+    from google.appengine.ext.webapp import WSGIApplication
+    url_map = []
+    url_map.append(("/api/session", UserInfo))
+    application = WSGIApplication(url_map, debug=True)
+    from google.appengine.ext.webapp.util import run_wsgi_app
+    run_wsgi_app(application)
