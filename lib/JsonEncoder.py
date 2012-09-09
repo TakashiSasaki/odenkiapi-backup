1b350840097b059533ef5e4f3255623fc72b6505
Takashi SASAKI <takashi316@gmail.com>
2012-09-09 10:06:44 +0900

    OAuthHmacToken causes deep recursion in dumps().

--- lib/JsonEncoder.py
+++ lib/JsonEncoder.py
@@ -1,5 +1,6 @@
 from json import JSONEncoder as _JSONEncoder
 from gaesessions import Session
+from gdata.gauth import OAuthHmacToken 
 
 from json import dumps as _dumps
 def dumps(d):
@@ -8,5 +9,7 @@ def dumps(d):
 class JSONEncoder(_JSONEncoder):
     def default(self,o):
         if isinstance(o, Session):
-            return str(o)
+            return str(o.sid)
+        if isinstance(o, OAuthHmacToken):
+            return o.token
         return JSONEncoder.default(self, o)
