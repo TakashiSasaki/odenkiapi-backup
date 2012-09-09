8cb2b345ac3ce83a8d539fa3201e81e60f8d7cb6
Takashi SASAKI <takashi316@gmail.com>
2012-09-09 23:42:49 +0900

    _Single and _DuplicationCheck were added in record/Data.py

--- record/Data.py
+++ record/Data.py
@@ -1,5 +1,6 @@
 from __future__ import unicode_literals, print_function
-from lib.JsonRpc import JsonRpcDispatcher, JsonRpcRequest, JsonRpcResponse
+from lib.JsonRpc import JsonRpcDispatcher, JsonRpcRequest, JsonRpcResponse, \
+    JsonRpcError
 from urlparse import urlparse
 from model.DataNdb import Data
 from google.appengine.ext import ndb
@@ -50,11 +51,50 @@ class _Data(JsonRpcDispatcher):
             self.response.out.write("keys = %s" % keys)
             return
         
+class _Single(JsonRpcDispatcher):
+    def GET(self, jrequest, jresponse):
+        assert isinstance(jrequest, JsonRpcRequest)
+        assert isinstance(jresponse, JsonRpcResponse)
+        jresponse.setId()
+        path_info = self.request.path_info.split("/")
+        data_id = int(path_info[3])
+        query = Data.querySingle(data_id)
+        data = query.get()
+        jresponse.addResult(data)
+        
+
+class _DuplicationCheck(JsonRpcDispatcher):
+    
+    def GET(self, jrequest, jresponse):
+        assert isinstance(jrequest, JsonRpcRequest)
+        assert isinstance(jresponse, JsonRpcResponse)
+        jresponse.setId()
+        try:
+            start = int(jrequest.getValue("start")[0])
+            end = int(jrequest.getValue("end")[0])
+        except:
+            jresponse.setError(JsonRpcError.INVALID_PARAMS, "start and end are mandatory")
+            return
+        query = Data.queryRange(start, end)
+        keys = query.fetch(keys_only=True)
+        for key in keys:
+            data = key.get()
+            assert isinstance(data, Data)
+            query_for_duplicated_data = data.queryDuplication()
+            assert isinstance(query_for_duplicated_data, ndb.Query)
+            keys_for_duplicated_data = query_for_duplicated_data.fetch(keys_only=True)
+            if len(keys_for_duplicated_data) <= 1: continue
+            jresponse.addResult([data.dataId, data.field, data.string, [keys_for_duplicated_data]])
+            
+        
+        
 if __name__ == "__main__":
     from google.appengine.ext.webapp import WSGIApplication
     mapping = []
     mapping.append(("/record/Data", _RecentData))
+    mapping.append(("/record/Data/[0-9]+", _Single))
     mapping.append(("/record/Data/[0-9]+/[0-9]+", _Data))
+    mapping.append(("/record/Data/duplicated", _DuplicationCheck))
     application = WSGIApplication(mapping, debug=True)
     from google.appengine.ext.webapp.util import run_wsgi_app
     run_wsgi_app(application)
