39a17383f18bd65ad648f3bd07189ace5db24488
Takashi SASAKI <takashi316@gmail.com>
2012-09-09 17:14:46 +0900

    wrapping by ndb.toplevel

--- lib/__init__.py
+++ lib/__init__.py
@@ -26,7 +26,10 @@ class WSGIApplication(_WSGIApplication):
     def __init__(self, *args, **keys):
         _WSGIApplication.__init__(self, *args, **keys)
         
-from google.appengine.ext.webapp.util import run_wsgi_app
+from google.appengine.ext.webapp.util import run_wsgi_app as _run_wsgi_app
+def run_wsgi_app(application):
+    from google.appengine.ext import ndb
+    _run_wsgi_app(ndb.toplevel(application))
 
 RPC_ERROR_USER_AGENT_DOES_NOT_ACCEPT_HTML = 1
 
