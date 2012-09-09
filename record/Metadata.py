e6aad0924abdaf2ab57b719ef0984c47a796e21a
Takashi SASAKI <takashi316@gmail.com>
2012-09-09 23:43:27 +0900

    Canonicalization of lists of Data entities in Metadata

--- record/Metadata.py
+++ record/Metadata.py
@@ -1,5 +1,7 @@
-from lib.JsonRpc import JsonRpcDispatcher, JsonRpcResponse
+from lib.JsonRpc import JsonRpcDispatcher, JsonRpcResponse, JsonRpcRequest, \
+    JsonRpcError
 from model.MetadataNdb import Metadata
+from model.DataNdb import Data, getCanonicalizedKey, canonicalizeKeyList
 
 class _Recent(JsonRpcDispatcher):
     def GET(self, jrequest, jresponse):
@@ -15,11 +17,42 @@ class _Recent(JsonRpcDispatcher):
 class _Range(JsonRpcDispatcher):
     def GET(self, jrequest, jresponse):
         pass
+    
+
+class _CleanData(JsonRpcDispatcher):
+    
+    def GET(self, jrequest, jresponse):
+        assert isinstance(jrequest, JsonRpcRequest)
+        assert isinstance(jresponse, JsonRpcResponse)
+        jresponse.setId()
+        try:
+            start = int(jrequest.getValue("start")[0])
+            end = int(jrequest.getValue("end")[0])
+        except Exception, e:
+            jresponse.setError(JsonRpcError.INVALID_PARAMS, unicode(e.__class__) + unicode(e))
+            return
+        query = Metadata.queryRange(start, end)
+        keys = query.fetch(keys_only=True)
+        #assert len(keys) == abs(start - end) + 1
+        for key in keys:
+            metadata = key.get()
+            assert isinstance(metadata, Metadata)
+            data_list = metadata.dataList
+            if not isinstance(data_list, list): continue
+            canonicalized_list= canonicalizeKeyList(data_list)
+            assert len(canonicalized_list) == len(data_list)
+            for i in range(len(canonicalized_list)):
+                assert canonicalized_list[i].get().field == data_list[i].get().field
+                assert canonicalized_list[i].get().string == data_list[i].get().string
+            metadata.dataList = canonicalized_list
+            metadata.put_async()
+            jresponse.addResult([metadata.metadataId, data_list, canonicalized_list])
 
 if __name__ == "__main__":
     mapping = []
     mapping.append(("/record/Metadata", _Recent))
     mapping.append(("/record/Metadata/[0-9]+/[0-9]+", _Range))
+    mapping.append(("/record/Metadata/CleanData", _CleanData))
     from google.appengine.ext.webapp import WSGIApplication
     application = WSGIApplication(mapping, debug=True)
     from google.appengine.ext.webapp.util import run_wsgi_app
