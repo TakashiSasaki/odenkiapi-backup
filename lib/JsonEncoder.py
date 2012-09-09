fe794aa5543cfceee5d816f4775ea5e7af3fc164
Takashi SASAKI <takashi316@gmail.com>
2012-09-09 16:29:18 +0900

    instances of NdbModel is jsonized by to_dict method

--- lib/JsonEncoder.py
+++ lib/JsonEncoder.py
@@ -1,8 +1,11 @@
 from json import JSONEncoder as _JSONEncoder
 from gaesessions import Session
-from gdata.gauth import OAuthHmacToken 
+from gdata.gauth import OAuthHmacToken
+from datetime import datetime 
+from google.appengine.ext import ndb
 
 from json import dumps as _dumps
+from model.NdbModel import NdbModel
 def dumps(d):
     return _dumps(d, indent=4, cls=JSONEncoder)
 
@@ -12,4 +15,11 @@ class JSONEncoder(_JSONEncoder):
             return str(o.sid)
         if isinstance(o, OAuthHmacToken):
             return o.token
+        if isinstance(o, datetime):
+            return o.isoformat()
+        if isinstance(o, ndb.Key):
+            assert isinstance(o, ndb.Key)
+            return unicode(o)
+        if isinstance(o, NdbModel):
+            return o.to_dict()
         return JSONEncoder.default(self, o)
