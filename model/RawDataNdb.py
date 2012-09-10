3d9dd7ae054c3824129b264d32d2e838cd8576d5
Takashi SASAKI <takashi316@gmail.com>
2012-09-11 04:33:53 +0900

    refactoring

--- model/RawDataNdb.py
+++ model/RawDataNdb.py
@@ -1,18 +1,9 @@
-#import lib
-#from urlparse import urlparse
 from google.appengine.ext import ndb
 #from google.appengine.ext.webapp import Request
 from model.Counter import Counter
-from json import dumps
+#from json import dumps
 from logging import debug
 from model.NdbModel import NdbModel
-#ctx = ndb.get_context()
-#lib.debug("ndb cache policy is %s" % ctx.get_cache_policy())
-#lib.debug("ndb memcache policy is %s" % ctx.get_memcache_policy())
-#lib.debug("ndb memcache timeout is %s" % ctx.get_memcache_timeout_policy())
-#ctx.set_memcache_policy(True)
-#ctx.set_cache_policy(True)
-#ctx.set_memcache_timeout_policy(7200)
 
 class RawData(NdbModel):
     rawDataId = ndb.IntegerProperty()
@@ -21,11 +12,11 @@ class RawData(NdbModel):
     query = ndb.StringProperty(indexed=False)
     fragment = ndb.StringProperty(indexed=False)
     body = ndb.StringProperty(indexed=False)
-    #_use_cache = True
-    #_use_memcache = True
-    #_memcache_timeout = 7200
 
     fieldnames = ["rawDataId", "path", "parameters", "query", "fragment", "body"]
+    
+    def to_list(self):
+        return [self.rawDataId, self.path, self.parameters, self.query, self.fragment, self.body]
     
     @classmethod
     def queryRange(cls, start, end):
@@ -33,14 +24,18 @@ class RawData(NdbModel):
         q = ndb.Query(kind="RawData")
         if start < end:
             q = q.order(cls.rawDataId)
-            q = q.filter(cls.rawDataId>=start)
-            q = q.filter(cls.rawDataId<=end)
+            q = q.filter(cls.rawDataId >= start)
+            q = q.filter(cls.rawDataId <= end)
             return q
         else:
             q = q.order(-cls.rawDataId)
-            q = q.filter(cls.rawDataId<=start)
-            q = q.filter(cls.rawDataId>=end)
+            q = q.filter(cls.rawDataId <= start)
+            q = q.filter(cls.rawDataId >= end)
             return q
+    
+    @classmethod
+    def fetchRange(cls, start, end, limit=100):
+        return cls.queryRange(start, end).fetch(keys_only=True, limit=100)
     
     @classmethod
     def queryRecent(cls):
@@ -48,6 +43,10 @@ class RawData(NdbModel):
         query = ndb.Query(kind="RawData")
         query = query.order(-cls.rawDataId)
         return query
+    
+    @classmethod
+    def fetchRecent(cls, limit=100):
+        return cls.queryRecent().fetch(keys_only=True, limit=limit)
 
     @classmethod
     def queryPeriod(cls, start_rawdata_id, end_rawdata_id):
@@ -64,8 +63,9 @@ class RawData(NdbModel):
             q = q.filter(cls.rawDataId >= start_rawdata_id)
         return q
 
-    def toDict(self):
-        return self.to_dict()
+    #def toDict(self):
+    #    return self.to_dict()
     
-    def toJson(self):
-        return dumps(self.to_dict()) 
+    #def toJson(self):
+    #    return dumps(self.to_dict())
+     
