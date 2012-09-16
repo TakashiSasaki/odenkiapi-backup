4a8fd638f267b2ff2a4e5eb9d356c1b8b3ba20a0
Takashi SASAKI <takashi316@gmail.com>
2012-09-16 10:58:25 +0900

    /_ah/stats works

--- appengine_config.py
+++ appengine_config.py
@@ -1,7 +1,11 @@
 # this code was from https://github.com/dound/gae-sessions/blob/master/README.markdown
 from __future__ import unicode_literals, print_function
-from gaesessions import SessionMiddleware
-from credentials import SESSION_SALT
 def webapp_add_wsgi_middleware(app):
+    from gaesessions import SessionMiddleware
+    from credentials import SESSION_SALT
     newapp = SessionMiddleware(app, cookie_key=SESSION_SALT)
+
+    from google.appengine.ext.appstats import recording
+    newapp = recording.appstats_wsgi_middleware(newapp)
+
     return newapp
