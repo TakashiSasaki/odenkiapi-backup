592eb185348116c98865b30ef0745d4e873483f4
Takashi SASAKI <takashi316@gmail.com>
2012-09-22 18:09:09 +0900

    lib.ui namespace was introduced to header.js

--- js/header.js
+++ js/header.js
@@ -1,8 +1,40 @@
-function abcde(){
-	alert("abcde");
-}
-function header(){
-	$("header").html('\
+var lib = lib || {}
+lib.ui = lib.ui || {}
+
+lib.ui.a = '<li>\
+                <a href="main.html"   data-ajax="false"   data-icon="home" >\
+                    <div class="ui-bar-a">\
+                        みんなのおでんき\
+                    </div>\
+                </a>\
+        </li>';
+    
+lib.ui.b = '<li >\
+                <a href="community.html"   data-ajax="false"   data-icon="star" >\
+                    <div class="ui-bar-b">\
+                        なかまのおでんき\
+                    </div>\
+                </a>\
+            </li>';
+    
+lib.ui.c = '<li>\
+                <a href="myself.html"   data-ajax="false"   data-icon="info">\
+                    <div class="ui-bar-c">\
+                        マイおでんき\
+                    </div>\
+                </a>\
+            </li>';
+            
+lib.ui.d = '<li>\
+                <a href="/auth/index.html"   data-ajax="false"   data-icon="gear" >\
+                    <div class="ui-bar-d">\
+                        設定\
+                    </div>\
+                </a>\
+            </li>';
+
+lib.ui.makeHeader = function(){
+	   $("header").html('\
 <div data-role="header" data-position="inline" data-theme="e" >\
 <h1>\
 <img width="40px" height="40px" src="https://twimg0-a.akamaihd.net/profile_images/1522990803/___________reasonably_small.png"></img>\
@@ -10,42 +42,27 @@ function header(){
 <img width="40px" height="40px" src="https://twimg0-a.akamaihd.net/profile_images/1522990803/___________reasonably_small.png"></img>\
 </h1>\
 </div>\
-	');
-}
+        ');
+}//header
 
-var a = '<li>\
-			<a href="main.html"   data-ajax="false"   data-icon="home" >\
-				<div class="ui-bar-a">\
-					みんなのおでんき\
-				</div>\
-			</a>\
-	</li>';
+lib.ui.makeNavbar = function(active){
+    	$("#navbar").html('<div data-role="navbar"><ul>'+lib.ui.a+lib.ui.b+lib.ui.c+lib.ui.d+'</ul></div>');
+    	$(".ui-bar-"+active).closest("a").addClass("ui-btn-active");
+}//makeNavbar
 
-var b = '<li >\
-			<a href="community.html"   data-ajax="false"   data-icon="star" >\
-				<div class="ui-bar-b">\
-					なかまのおでんき\
-				</div>\
-			</a>\
-		</li>';
+lib.ui.showElement=function(selector){
+				$(selector).each(function() {
+					$(this).css("display", "block");
+					$(this).css("background", "lightgreen");
+					$(this).css("margin", "1px");
+				});
+}//showElement
 
-var c = '<li>\
-			<a href="myself.html"   data-ajax="false"   data-icon="info">\
-				<div class="ui-bar-c">\
-					マイおでんき\
-				</div>\
-			</a>\
-		</li>';
-		
-var d = '<li>\
-			<a href="/auth/index.html"   data-ajax="false"   data-icon="gear" >\
-				<div class="ui-bar-d">\
-					設定\
-				</div>\
-			</a>\
-		</li>';
+lib.ui.hideElement = function (selector){
+				$(selector).each(function() {
+					$(this).css("display", "none");
+					$(this).css("background", "lightgreen");
+					$(this).css("margin", "1px");
+				});
+}//hideElement
 
-function navbar(active){
-	$("#navbar").html('<div data-role="navbar"><ul>'+a+b+c+d+'</ul></div>');
-	$(".ui-bar-"+active).closest("a").addClass("ui-btn-active");
-}
