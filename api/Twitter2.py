164a3ef8d2fa3dfacb773e0a789540e017748a80
Takashi SASAKI <takashi316@gmail.com>
2012-09-25 11:15:03 +0900

    OAuth dance with  Twitter in api/Twitter2.py

--- api/Twitter2.py
+++ api/Twitter2.py
@@ -4,7 +4,6 @@ from lib.json.JsonRpcRequest import JsonRpcRequest
 from lib.json.JsonRpcResponse import JsonRpcResponse
 from credentials import TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET
 from model.TwitterUser import TwitterUser
-from model.OdenkiUser import OdenkiUser
 from lib.json.JsonRpcError import EntityNotFound, MixedAuthentication, \
     OAuthError
 import httplib2
@@ -27,13 +26,27 @@ class Twitter2(JsonRpcDispatcher):
         assert isinstance(jrequest, JsonRpcRequest)
         assert isinstance(jresponse, JsonRpcResponse)
         jresponse.setId()
-        try:
-            twitter_user = TwitterUser.loadFromSession()
-            assert isinstance(twitter_user, TwitterUser)
-            jresponse.setResultValue("twitterId", twitter_user.twitterId)
-            jresponse.setResultValue("screenName", twitter_user.screenName)
-            jresponse.setResultValue("odenkiId", twitter_user.odenkiId)
-        except EntityNotFound: pass
+        twitter_user = TwitterUser.loadFromSession()
+        assert isinstance(twitter_user, TwitterUser)
+        jresponse.setResultValue("twitterId", twitter_user.twitterId)
+        jresponse.setResultValue("screenName", twitter_user.screenName)
+        jresponse.setResultValue("odenkiId", twitter_user.odenkiId)
+        jresponse.setResultValue("name", twitter_user.name)
+        jresponse.setResultValue("locatioin", twitter_user.location)
+        jresponse.setResultValue("profile_image_url", twitter_user.profile_image_url)
+        jresponse.setResultValue("profile_image_url_https", twitter_user.profile_image_url_https)
+        jresponse.setResultValue("description", twitter_user.description)
+        jresponse.setResultValue("time_zone", twitter_user.time_zone)
+        jresponse.setResultValue("url", twitter_user.url)
+        jresponse.setResultValue("utc_offset", twitter_user.utc_offset)
+        
+    def deleteAll(self, jrequest, jresponse):
+        assert isinstance(jrequest, JsonRpcRequest)
+        assert isinstance(jresponse, JsonRpcResponse)
+        jresponse.setId()
+        TwitterUser.deleteAll()
+
+
         
 REQUEST_TOKEN_SESSION_KEY = ";klsuioayggahihiaoheiajfioea"
 REQUEST_TOKEN_SECRET_SESSION_KEY = "gscfgnhbfvcfscgdfgiubH"
@@ -100,16 +113,45 @@ class OAuthCallback(JsonRpcDispatcher):
         try:
             access_token = access_token_dict["oauth_token"]
             access_token_secret = access_token_dict["oauth_token_secret"]
+            user_id = access_token_dict["user_id"]
+            screen_name = access_token_dict["screen_name"]
         except KeyError:
             raise OAuthError("OAuthCallback failed to exchange verified request token to access token.")
         jresponse.setResultValue("access_token", access_token)
         jresponse.setResultValue("access_token_secret", access_token_secret)
+        jresponse.setResultValue("uer_id", user_id)
+        jresponse.setResultValue("screen_name", screen_name)
         jresponse.setResultValue("request_token", request_token)
         jresponse.setResultValue("request_token_secret", request_token_secret)
         jresponse.setResultValue("request_token_verified", oauth_token)
         jresponse.setResultValue("oauth_verifier", oauth_verifier)
         
-        jresponse.setRedirectTarget("/api/Twitter2")
+        callback_twitter_user = TwitterUser()
+        callback_twitter_user.setAccessToken(access_token, access_token_secret)
+        callback_twitter_user.verifyCredentials11()
+        callback_twitter_user.saveToSession()
+
+        from lib.Session import fillUser
+        fillUser()
+        
+        try:
+            twitter_user = TwitterUser.loadFromSession()
+            assert isinstance(twitter_user, TwitterUser)
+            twitter_user.overrideBy(callback_twitter_user)
+            twitter_user.put()
+            twitter_user.saveToSession()
+        except EntityNotFound: pass
+        
+        try:
+            twitter_user = TwitterUser.getByTwitterId(callback_twitter_user.twitterId)
+            assert isinstance(twitter_user, TwitterUser)
+            twitter_user.overrideBy(callback_twitter_user)
+            twitter_user.put()
+            twitter_user.saveToSession()
+        except EntityNotFound:
+            callback_twitter_user.put()
+            callback_twitter_user.saveToSession()
+        fillUser()
         
 if __name__ == "__main__":
     mapping = []
