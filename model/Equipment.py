0d2b69ca77c106c53f80e5f347a3eeee5d56a9ee
Takashi SASAKI <takashi316@gmail.com>
2012-10-23 05:17:47 +0900

    implementing class Equipment

--- model/Equipment.py
+++ model/Equipment.py
@@ -1,54 +1,31 @@
-from  google.appengine.ext import db
-from  google.appengine.ext.webapp import Request, RequestHandler, WSGIApplication
-from google.appengine.ext.webapp.util import run_wsgi_app
-from urlparse import urlparse
-import logging
-from model.Sender import  Sender
-from model.RawData import RawData
-from model.Data import Data
-import datetime
+#!-*- coding:utf-8 -*-
+from __future__ import unicode_literals, print_function
 from model.Counter import Counter
-from google.appengine.api import memcache
-from google.appengine.ext.webapp import  template
+from google.appengine.ext import ndb
+from lib.json.JsonRpcError import EntityDuplicated, EntityNotFound
 
+class Equipment(ndb.Model):
+    equipmentId = ndb.IntegerProperty()
+    senderIds = ndb.IntegerProperty(repeated=True)
+    equipmentName = ndb.StringProperty(indexed=False)
+    odenkiId = ndb.IntegerProperty()
+    field = ndb.StringProperty()
+    string = ndb.StringProperty()
 
-class Equipment(db.Model):
-    equipmentId = db.IntegerProperty()
-    senderIds = db.ListProperty(int)
-    equipmentName = db.StringProperty()
+    @classmethod
+    def createNew(cls, odenki_id, field, string):
+        equipment = Equipment()
+        equipment.equipmentId = Counter.GetNextId("equipmentId")
+        equipment.odenkiId = odenki_id
+        equipment.put()
     
     @classmethod
     def getByEquipmentId(cls, equipment_id):
-        query = Equipment.all()
-        query.filter("equipmentId = ", equipment_id)
-        result = query.run()
-        equipment  = result.next()
-        return equipment
-    
-    def putByEquipmentId(self):
-        equipment = Equipment.getByEquipmentId(self.equipmentId)
-        if equipment is None:
-            self.put()
-        else:
-            equipment.senderIds = self.senderIds
-            equipment.equipmentName = self.equipmentName
-
-class _RequestHandler(RequestHandler):
-    def get(self):
-        xx = []
-        for x in Equipment.all():
-            x = {"equipmentId": x.equipmentId,
-                 "equipmentName": x.equipmentName,
-                 "senderIds": x.senderIds}
-            xx.append(x)
-        self.response.out.write(template.render("html/Equipment.html", {"xx":xx}))
-        
-if __name__ == "__main__":
-    x = Equipment()
-    x.equipmentId = 100
-    x.senderIds = [100]
-    x.equipmentName = "test equipment"
-    x.putByEquipmentId()
-    logging.getLogger().setLevel(logging.DEBUG)
-    application = WSGIApplication([('/Equipment', _RequestHandler)], debug=True)
-    run_wsgi_app(application)
+        query = ndb.Query(kind="Equipment")
+        query = query.filter(Equipment.equipmentId == equipment_id)
+        keys = query.get(keys_only=True, limit=2)
+        if len(keys >= 2):
+            raise EntityDuplicated({"equipmentId":equipment_id})
+        if len(keys == 0):
+            raise EntityNotFound({"equipmentId":equipment_id})
+        return keys[0].get()
