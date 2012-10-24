ae1221cb55ed6cccdb226b18877b32c06a4e7a92
Takashi SASAKI <takashi316@gmail.com>
2012-10-24 10:09:50 +0900

    C:/Users/Takashi/AppData/Local/GitHub/PortableGit_8810fd5c2c79c73adcc73fd0825f3b32fdb816e7/api/Sensor works for method=register

---
+++ api/Sensor.py
@@ -0,0 +1,87 @@
+#!-*- coding:utf-8 -*-
+from __future__ import unicode_literals, print_function
+from lib.gae.JsonRpcDispatcher import JsonRpcDispatcher
+from lib.json.JsonRpcRequest import JsonRpcRequest
+from lib.json.JsonRpcResponse import JsonRpcResponse
+from google.appengine.ext import ndb
+from model.Sensor import Sensor
+from google.appengine.ext.webapp import Request
+from lib.gae import run_wsgi_app
+from lib.json.JsonRpcError import InvalidParams, EntityExists, EntityNotFound
+from model.DataNdb import Data
+from gaesessions import get_current_session
+from model.OdenkiUser import OdenkiUser
+from test.test_pprint import uni
+
+class Enumerate(JsonRpcDispatcher):
+    def GET(self, jrequest, jresponse):
+        assert isinstance(jrequest, JsonRpcRequest)
+        assert isinstance(jresponse, JsonRpcResponse)
+        jresponse.setId()
+        query = ndb.Query(kind="Sensor")
+        query = query.order(-Sensor.uniqueSensorId)
+        keys = query.fetch(keys_only=True, limit=100)
+        for key in keys:
+            jresponse.addResult(key.get())
+            
+    def register(self, jrequest, jresponse):
+        assert isinstance(jrequest, JsonRpcRequest)
+        assert isinstance(jresponse, JsonRpcResponse)
+        jresponse.setId()
+        request = jrequest.request
+        assert isinstance(request, Request)
+        if len(request.params) <3:
+            raise InvalidParams("method=register&x=y&z=w&moduleId=mid&sensorId=sid&sensorName=mysensor where x is product field, y is product string, z is serial field, is serial string")
+        assert request.params.items()[0][0] == "method"
+        assert request.params.items()[0][1] == "register"
+        (product_name_field, product_name_string) = request.params.items()[1]
+        (serial_number_field, serial_number_string) = request.params.items()[2]
+        assert isinstance(product_name_field, str)
+        product_name_field = unicode(product_name_field) 
+        assert isinstance(product_name_field, unicode)
+        assert isinstance(serial_number_field, str)
+        serial_number_field = unicode(serial_number_field)
+        assert isinstance(serial_number_string, unicode)
+        try:
+            product_name_data = Data.prepare(product_name_field, product_name_string)
+            serial_number_data = Data.prepare(serial_number_field, serial_number_string)
+            module_id_string = request.get("moduleId")
+            assert len(module_id_string) > 0
+            sensor_id_string = request.get("sensorId")
+            assert len(sensor_id_string) > 0
+            module_id_data = Data.prepare("moduleId", request.get("moduleId"))
+            sensor_id_data = Data.prepare("sensorId", request.get("sensorId"))
+            existing_sensor = Sensor.getByProductNameSerialNumberModuleIdSensorId(product_name_data, serial_number_data, module_id_data, sensor_id_data)
+            raise EntityExists("Equipment", {product_name_field:product_name_string, serial_number_field: serial_number_string, "moduleId":module_id_string, "sensorId":sensor_id_string})
+        except EntityNotFound: pass
+        odenki_user = OdenkiUser.loadFromSession()
+        assert isinstance(odenki_user, OdenkiUser)
+        sensor_name_data = Data.prepare("sensorName", request.get("sensorName"))
+        sensor = Sensor.create(product_name_data, serial_number_data, module_id_data, sensor_id_data, sensor_name_data, odenki_user)
+        jresponse.addResult(sensor)
+
+class OneHour(JsonRpcDispatcher):
+    def GET(self, jrequest, jresponse):
+        assert isinstance(jrequest, JsonRpcRequest)
+        assert isinstance(jresponse, JsonRpcResponse)
+        pass
+
+class Recent(JsonRpcDispatcher):
+    def GET(self, jrequest, jresponse):
+        assert isinstance(jrequest, JsonRpcRequest)
+        assert isinstance(jresponse, JsonRpcResponse)
+        pass
+    
+class Register(JsonRpcDispatcher):
+    def GET(self, jrequest, jresponse):
+        assert isinstance(jrequest, JsonRpcRequest)
+        assert isinstance(jresponse, JsonRpcResponse)
+        pass
+
+if __name__ == "__main__":
+    mapping = []
+    mapping.append(("/api/Sensor", Enumerate))
+    mapping.append(("/api/Sensor/[0-9]+", Recent))
+    mapping.append(("/api/Sensor/[0-9]+/[0-9][0-9][0-9][0-9]/[0-9]+/[0-9]+/[0-9]+", OneHour))
+    mapping.append(("/api/Sensor/register", Register))
+    run_wsgi_app(mapping)
--- model/Sensor.py
+++ model/Sensor.py
@@ -5,6 +5,7 @@ from google.appengine.ext import ndb
 from lib.json.JsonRpcError import EntityDuplicated, EntityNotFound, EntityExists
 from model.DataNdb import Data
 from model.NdbModel import NdbModel
+from model.OdenkiUser import OdenkiUser
 
 class Sensor(NdbModel):
     uniqueSensorId = ndb.IntegerProperty()
@@ -20,46 +21,41 @@ class Sensor(NdbModel):
     #string = ndb.StringProperty()
 
     @classmethod
-    def create(cls, product_name_tuple, serial_number_tuple, module_id_tuple, sensor_id_tuple, sensor_name_tuple, odenki_id):
-        assert isinstance(odenki_id, int)
-        assert isinstance(product_name_tuple, tuple)
-        assert isinstance(serial_number_tuple, tuple)
-        assert isinstance(module_id_tuple, tuple)
-        assert isinstance(sensor_id_tuple, tuple)
-        assert isinstance(sensor_name_tuple, tuple)
+    def create(cls, product_name_data, serial_number_data, module_id_data, sensor_id_data, sensor_name_data, odenki_user):
+        assert isinstance(odenki_user, OdenkiUser)
+        assert isinstance(product_name_data, Data)
+        assert isinstance(serial_number_data, Data)
+        assert isinstance(module_id_data, Data)
+        assert isinstance(sensor_id_data, Data)
+        assert isinstance(sensor_name_data, Data)
         
-        product_name = Data.prepare(product_name_tuple)
-        serial_number = Data.prepare(serial_number_tuple)
-        module_id = Data.prepare(module_id_tuple)
-        sensor_id = Data.prepare(sensor_id_tuple)
-        sensor_name = Data.prepare(sensor_name_tuple)
         try:
-            existing_sensor = cls.getByProductAndSerial(product_name, serial_number, module_id, sensor_id)
-            raise EntityExists("Equipment", {"product_name":product_name_tuple, "serial_number": serial_number_tuple, "module_id": module_id_tuple, "sensor_id": sensor_id_tuple })
+            existing_sensor = cls.getByProductNameSerialNumberModuleIdSensorId(product_name_data, serial_number_data, module_id_data, sensor_id_data)
+            raise EntityExists("Equipment", {"product_name_data":product_name_data, "serial_number_data": serial_number_data, "module_id_data": module_id_data, "sensor_id_data": sensor_id_data })
         except: 
             sensor = Sensor()
             sensor.uniqueSensorId = Counter.GetNextId("equipmentId")
-            sensor.productName = product_name
-            sensor.serialNumber = serial_number
-            sensor.moduleId = module_id
-            sensor.sensorId = sensor_id
-            sensor.odenkiId = odenki_id
+            sensor.productName = product_name_data.dataId
+            sensor.serialNumber = serial_number_data.dataId
+            sensor.moduleId = module_id_data.dataId
+            sensor.sensorId = sensor_id_data.dataId
+            sensor.odenkiId = odenki_user.odenkiId
             sensor.put()
             return sensor
     
     @classmethod
-    def getByProductAndSerial(cls, product_name, serial_number, module_id, sensor_id, sensor_name, odenki_id):
-        assert isinstance(product_name, int) and isinstance(serial_number, int) and isinstance(module_id, int)and isinstance(sensor_id, int)and isinstance(sensor_name, int) and isinstance(odenki_id, int)
+    def getByProductNameSerialNumberModuleIdSensorId(cls, product_name_data, serial_number_data, module_id_data, sensor_id_data):
+        assert isinstance(product_name_data, Data) and isinstance(serial_number_data, Data) and isinstance(module_id_data, Data)and isinstance(sensor_id_data, Data)
         query = ndb.Query(kind="Sensor")
-        query = query.filter(cls.productName == product_name)
-        query = query.filter(cls.serialNumber == serial_number)
-        query = query.filter(cls.moduleId == module_id)
-        query = query.filter(cls.sensorId == sensor_id)
+        query = query.filter(cls.productName == product_name_data.dataId)
+        query = query.filter(cls.serialNumber == serial_number_data.dataId)
+        query = query.filter(cls.moduleId == module_id_data.dataId)
+        query = query.filter(cls.sensorId == sensor_id_data.dataId)
         keys = query.fetch(keys_only=True, limit=2)
         if len(keys) == 0:
-            raise EntityNotFound("Sensor", {"product_name":product_name, "serial_number":serial_number, "module_id": module_id, "sensor_id":sensor_id})
+            raise EntityNotFound("Sensor", {"product_name_data":product_name_data, "serial_number_data":serial_number_data, "module_id_data": module_id_data, "sensor_id_name":sensor_id_data})
         if len(keys) == 2:
-            raise EntityDuplicated({"product_name":product_name, "serial_number":serial_number, "module_id":module_id, "sensor_id":sensor_id})
+            raise EntityDuplicated({"product_name_data":product_name_data, "serial_number_data":serial_number_data, "module_id_name":module_id_data, "sensor_id_data":sensor_id_data})
         return keys[0].get()
 
     @classmethod
